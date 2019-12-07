f3556336597a8cec8d205191cc1585e8acd2f70f
Christian <chr.revel@gmail.com>
2019-12-07 09:50:41 -0700

    Connected app to mongodb, created tours model and worked on tours get all, get one and create controller methods

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,69 +1,71 @@
-const fs = require('fs');
+const Tour = require('../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-// middleware function to make sure ID is valid.
-exports.checkID = (req, res, next, id) => {
-  if (id > tours.length) {
-    return res.status(404).json({ status: 'Fail', message: 'Invalid ID' });
-  }
-  next();
-};
-
-exports.validateBody = (req, res, next) => {
-  const { price, name } = req.body;
-  if (!price || !name) {
-    return res.status(400).json({
-      status: 'Fail',
-      message:
-        'Body does not have appropriate fields. Please ensure name and price of tour are included.'
-    });
-  }
-  next();
-};
+// Middleware to validate body...
+// exports.validateBody = (req, res, next) => {
+//   const { price, name } = req.body;
+//   if (!price || !name) {
+//     return res.status(400).json({
+//       status: 'Fail',
+//       message:
+//         'Body does not have appropriate fields. Please ensure name and price of tour are included.'
+//     });
+//   }
+//   next();
+// };
 
 // Get all tours in database
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
       tours
-    }
-  });
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Fail',
+      message: err
+    });
+  }
 };
 
 // Get a single tour depending on ID
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({_id: req.params.id})
+    res.status(200).json({
+      status: 'success',
       tour
-    }
-  });
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Fail',
+      message: err
+    });
+  }
 };
 
 // Create New Tour
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = { id: newId, ...req.body };
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).send({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
+exports.createTour = async (req, res) => {
+  // const newTour = new Tour(req.body);
+  // newTour.save()
+  try {
+    const newTour = await Tour.create(req.body);
+    newTour.save();
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent.'
+    });
+  }
 };
 
 // Update an existing tour
